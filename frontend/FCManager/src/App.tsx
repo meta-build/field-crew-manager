bc2bbeb1435814212d0fa886fa0907ebfb9e8aba
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-09-09 22:36:22 -0300

    chore: init react native project

---
+++ frontend/FCManager/src/App.tsx
@@ -0,0 +1,12 @@
+import {SafeAreaView, Text} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Teste</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
