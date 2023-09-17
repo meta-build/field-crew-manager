3d88b8d3f0478054811fb7768010b2c2fc707cf5
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-09-17 15:44:10 -0300

    feat: new equipment type request

--- backend/src/controllers/TipoController.ts
+++ backend/src/controllers/TipoController.ts
@@ -7,12 +7,29 @@ class TipoController {
       // obs.: armazenar no array abaixo
       const tipos = [];
 
-      
+
 
       return res.status(tipos.length ? 200 : 204).json(tipos);
     } catch (err) {
       return res.status(500).json({ error: err });
     }
+  }
+
+  public async new (req: Request, res: Response) {
+    console.log(req.body);
+    const { name } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ error: 'Campo "value" não informado.' });
+    }
+
+    // lógica para inserir novo tipo de equipamento com a variável value
+
+
+    // após inserção, pegar id recém criado e armazenar na variável abaixo
+    const id = '123123';
+
+    return res.status(201).json({ id });
   }
 }
 
--- backend/src/routes/tipo.ts
+++ backend/src/routes/tipo.ts
@@ -4,5 +4,6 @@ import TipoController from "../controllers/TipoController";
 const routes = Router();
 
 routes.get('/tipos', TipoController.getTipos);
+routes.post('/tipos', TipoController.new);
 
 export default routes;
