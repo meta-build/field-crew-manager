791079a7652cedb2111507b8787d98d790e292dd
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-09-17 15:41:57 -0300

    fix: requests json error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,6 +5,8 @@ import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 app.use(cors<Request>);
 app.listen(8080, () => console.log('rodando na porta 8080'));
