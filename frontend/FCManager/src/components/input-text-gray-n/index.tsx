d39c4401241e3fcf38c6403bf2d19e7ae68a6ba0
Wallace Marinho de Souza Silva <wallace.marinhosouzas@gmail.com>
2023-09-17 18:17:14 -0300

    feat: Input-text-gray-normal-component

---
+++ frontend/FCManager/src/components/input-text-gray-n/index.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { NativeSyntheticEvent, SafeAreaView, StyleSheet, TextInput, TextInputChangeEventData } from "react-native";
+
+const Inputgraynormal = () => {
+
+    const [value,Setvalue] = useState<string>('') 
+    const handleOnChangeInput = (event: NativeSyntheticEvent<TextInputChangeEventData>) => {
+        Setvalue(event.nativeEvent.text);
+    };
+
+    return (
+    <SafeAreaView>
+        <TextInput onChange={handleOnChangeInput}
+         value={value}
+         style={styles.inputgray}
+         placeholder="Pesquisar por tipo..."
+         placeholderTextColor="gray"
+       
+       />
+    </SafeAreaView>
+    );
+  };
+
+  const styles = StyleSheet.create({
+
+
+    inputgray: {
+        backgroundColor: '#BFBFBF',
+        color:"black",
+        borderRadius: 10
+    }
+  });
+
+
+  export default Inputgraynormal; 
