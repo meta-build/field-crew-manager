5efc40d7493d2fc2274881e3b1054b2c4ecceb92
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-09-18 19:12:29 -0300

    feat: enable props of TextInput component

--- frontend/FCManager/src/components/InputText/index.tsx
+++ frontend/FCManager/src/components/InputText/index.tsx
@@ -1,54 +1,49 @@
 import React from 'react';
-import {useState} from 'react';
 import {
-  NativeSyntheticEvent,
   SafeAreaView,
   StyleSheet,
   TextInput,
-  TextInputChangeEventData,
+  TextInputProps,
 } from 'react-native';
 import colors from '../../styles/variables';
 
-interface Props {
+interface Props extends TextInputProps {
   color: 'gray' | 'white';
+  enable?: boolean;
 }
 
-const InputText = (props: Props) => {
-  const [value, Setvalue] = useState<string>('');
-  const handleOnChangeInput = (
-    event: NativeSyntheticEvent<TextInputChangeEventData>,
-  ) => {
-    Setvalue(event.nativeEvent.text);
+const InputText = ({enable = true, color, ...props}: Props) => {
+  const inputStyles = {
+    borderRadius: 10,
+    padding: 6,
+    color: colors.dark_gray,
   };
+
+  const styles = StyleSheet.create({
+    gray: {
+      ...inputStyles,
+      backgroundColor: colors.white_2,
+      ...(!enable ? {backgroundColor: colors.white_3} : {}),
+    },
+    white: {
+      ...inputStyles,
+      backgroundColor: colors.white,
+      ...(!enable ? {backgroundColor: colors.white_4} : {}),
+    },
+  });
 
   return (
     <SafeAreaView>
       <TextInput
-        onChange={handleOnChangeInput}
-        value={value}
-        style={styles[props.color]}
-        placeholder="Pesquisar por tipo..."
+        style={styles[color]}
         placeholderTextColor={colors.light_gray_2}
+        onChange={props.onChange}
+        value={props.value}
+        placeholder={props.placeholder}
+        editable={enable}
       />
     </SafeAreaView>
   );
 };
-
-const inputStyles = {
-  borderRadius: 10,
-  padding: 12,
-  color: colors.dark_gray,
-};
-
-const styles = StyleSheet.create({
-  gray: {
-    ...inputStyles,
-    backgroundColor: colors.white_2,
-  },
-  white: {
-    ...inputStyles,
-    backgroundColor: colors.white,
-  },
-});
 
 export default InputText;
--- frontend/FCManager/src/styles/variables.ts
+++ frontend/FCManager/src/styles/variables.ts
@@ -1,8 +1,10 @@
 const colors = {
   white: '#FFFFFF',
   white_2: '#F3F3F3',
+  white_3: '#DEDEDE',
   white_4: '#DFDFDF',
   light_gray_2: '#BFBFBF',
+  gray: '#AFAFAF',
   gray_2: '#909090',
   dark_gray: '#656565',
 };
