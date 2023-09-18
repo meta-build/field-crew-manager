d2d61012441aee5d8be87026eb5f03bc77ea64a3
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-09-18 10:05:11 -0300

    feat: add colors variables

---
+++ frontend/FCManager/src/styles/variables.ts
@@ -0,0 +1,10 @@
+const colors = {
+  white: '#FFFFFF',
+  white_2: '#F3F3F3',
+  white_4: '#DFDFDF',
+  light_gray_2: '#BFBFBF',
+  gray_2: '#909090',
+  dark_gray: '#656565',
+};
+
+export default colors;
