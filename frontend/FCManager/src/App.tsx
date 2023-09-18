c0fa5613c80aeb0b907ee6dbd0442f2eb09ec3d7
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-09-18 08:39:36 -0300

    refactor: change input component name

--- frontend/FCManager/src/App.tsx
+++ frontend/FCManager/src/App.tsx
@@ -1,11 +1,11 @@
 import {SafeAreaView} from 'react-native';
 import React from 'react';
-import Inputgraynormal from './components/InputText';
+import InputText from './components/InputText';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Inputgraynormal />
+      <InputText />
     </SafeAreaView>
   );
 };
