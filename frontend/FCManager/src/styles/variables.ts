cab8abe012580e1753ef00860c5cab3a908aa81e
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-09-23 23:57:50 -0300

    feat: insert colors

--- frontend/FCManager/src/styles/variables.ts
+++ frontend/FCManager/src/styles/variables.ts
@@ -3,10 +3,13 @@ const colors = {
   white_2: '#F3F3F3',
   white_3: '#DEDEDE',
   white_4: '#DFDFDF',
+  light_gray_1: '#E1E1E1',
   light_gray_2: '#BFBFBF',
   gray: '#AFAFAF',
   gray_2: '#909090',
   dark_gray: '#656565',
+  green_1: '#81DC0D',
+  green_2: '#579607',
 };
 
 export default colors;
