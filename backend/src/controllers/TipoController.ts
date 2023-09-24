3a55441feb549f2b613033efa1848632596d86c1
William Claudimar <93256317+William2819@users.noreply.github.com>
2023-09-23 23:35:45 -0300

    add-create-get-tipo

--- backend/src/controllers/TipoController.ts
+++ backend/src/controllers/TipoController.ts
@@ -1,35 +1,34 @@
 import { Request, Response } from "express";
+import typeSchema from "../models/typeSchema";
 
 class TipoController {
   public async getTipos (req: Request, res: Response) {
-    try {
       // lógica para retornar todos os tipos de equipamentos no bd
       // obs.: armazenar no array abaixo
-      const tipos = [];
-
-
-
-      return res.status(tipos.length ? 200 : 204).json(tipos);
-    } catch (err) {
-      return res.status(500).json({ error: err });
+    try{
+      const type = await typeSchema.find();
+      return res.json(type);
+    } catch (error) {
+      res.status(400).json({
+        error: "something wrong happened",
+        message: error
+      });
     }
   }
 
   public async new (req: Request, res: Response) {
-    console.log(req.body);
-    const { name } = req.body;
-
-    if (!name) {
-      return res.status(400).json({ error: 'Campo "value" não informado.' });
-    }
-
-    // lógica para inserir novo tipo de equipamento com a variável value
-
-
-    // após inserção, pegar id recém criado e armazenar na variável abaixo
-    const id = '123123';
-
-    return res.status(201).json({ id });
+    const { tipo, serial, cidade, obs } = req.body;
+    // lógica para retornar todos os tipos de equipamentos no bd
+    // obs.: armazenar no array abaixo
+  try{
+    const type = await typeSchema.create({tipo, serial, cidade,obs});
+    return res.json(type);
+  } catch (error) {
+    res.status(400).json({
+      error: "something wrong happened",
+      message: error
+    });
+  }
   }
 }
 
