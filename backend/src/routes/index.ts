bec94b5ac4d9a6a630ec28440341907bc65a07ca
Rodrigo Diniz Goulart <90328897+RodrigoDGoulart@users.noreply.github.com>
2023-09-24 00:39:59 -0300

    Revert "Feat/create-and-get-component-backend"

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,11 +1,7 @@
 import { Router, Request, Response } from 'express';
-import equipamentos from './equipamento';
-import tipo from './tipo';
 
 const routes = Router();
 
-routes.use(equipamentos);
-routes.use(tipo);
-routes.use((req: Request, res: Response) => res.status(404).json({ error: 'Requisição desconhecida.' }));
+routes.get('/teste', (req: Request, res: Response) => res.json({ test: 'teste de rota' }));
 
 export default routes;
