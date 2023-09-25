e539a1a1f35aa74308a185c7d2c3033285f2db5b
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-09-24 22:54:58 -0300

    feat: modal filter in ToolList screen

--- frontend/FCManager/src/screens/ToolList/index.tsx
+++ frontend/FCManager/src/screens/ToolList/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Dimensions,
   FlatList,
   SafeAreaView,
   StyleSheet,
+  Text,
   View,
 } from 'react-native';
 import Header from '../../components/Header/Index';
@@ -12,12 +13,20 @@ import InputText from '../../components/InputText';
 import Btn from '../../components/Button';
 import FilterIcon from '../../assets/icons/filterGreen.svg';
 import ToolItem from '../../components/ToolItem';
+import BottomModal from '../../components/BottomModal';
+import Title from '../../components/Title';
+import Dropdown from '../../components/Dropdown';
 
 const {width, height} = Dimensions.get('window');
 
 function ToolList({navigation}: any) {
+  const [filterModal, setFilterModal] = useState(false);
+
+  const [city, setCity] = useState('');
+  const [status, setStatus] = useState('');
+
   const openFilter = () => {
-    console.log('filter');
+    setFilterModal(true);
   };
 
   const openNewTool = () => {
@@ -26,6 +35,16 @@ function ToolList({navigation}: any) {
 
   const openItem = (serie: string) => {
     navigation.navigate('ToolProfile', {id: serie});
+  };
+
+  const confirmFilter = () => {
+    setFilterModal(false);
+  };
+
+  const cancelFilter = () => {
+    setCity('');
+    setStatus('');
+    setFilterModal(false);
   };
 
   const lista: {
@@ -48,46 +67,106 @@ function ToolList({navigation}: any) {
     },
   ];
 
+  const cities = [{label: 'sjc', value: 'sjc'}];
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Header text="Equipamentos" />
-      <View style={styles.content}>
-        <View style={styles.searchView}>
-          <InputText
-            placeholder="Pesquisar por tipo"
-            style={styles.searchInput}
-            color="white"
-          />
+    <>
+      <SafeAreaView style={styles.container}>
+        <Header text="Equipamentos" />
+        <View style={styles.content}>
+          <View style={styles.searchView}>
+            <InputText
+              placeholder="Pesquisar por tipo"
+              style={styles.searchInput}
+              color="white"
+            />
+            <View style={styles.filterView}>
+              <Btn
+                onPress={() => openFilter()}
+                styleType="blank"
+                icon={<FilterIcon width={18} height={15} />}
+              />
+              {city && status ? (
+                <Text style={styles.filterBadge}>2</Text>
+              ) : city || status ? (
+                <Text style={styles.filterBadge}>1</Text>
+              ) : (
+                <></>
+              )}
+            </View>
+          </View>
           <Btn
-            onPress={() => openFilter()}
-            styleType="blank"
-            icon={<FilterIcon width={18} height={15} />}
+            onPress={() => openNewTool()}
+            styleType="filled"
+            title="Adicionar novo equipamento"
+          />
+          <FlatList
+            data={lista}
+            renderItem={({item}) => (
+              <View style={styles.item}>
+                <ToolItem
+                  tool={{
+                    img_uri: item.img,
+                    n_serie: item.serie,
+                    status: item.status === 'ativo' ? 'active' : 'deactive',
+                    tipoLabel: item.tipo,
+                  }}
+                  onPress={() => openItem(item.serie)}
+                />
+              </View>
+            )}
+            keyExtractor={item => item.serie}
           />
         </View>
-        <Btn
-          onPress={() => openNewTool()}
-          styleType="filled"
-          title="Adicionar novo equipamento"
-        />
-        <FlatList
-          data={lista}
-          renderItem={({item}) => (
-            <View style={styles.item}>
-              <ToolItem
-                tool={{
-                  img_uri: item.img,
-                  n_serie: item.serie,
-                  status: item.status === 'ativo' ? 'active' : 'deactive',
-                  tipoLabel: item.tipo,
-                }}
-                onPress={() => openItem(item.serie)}
-              />
-            </View>
-          )}
-          keyExtractor={item => item.serie}
-        />
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+      <BottomModal
+        onPressOutside={() => setFilterModal(false)}
+        visible={filterModal}>
+        <Title color="green" text="Filtros" align="center" />
+        <View style={styles.filterFields}>
+          <View>
+            <Text style={styles.label}>Cidade</Text>
+            <Dropdown
+              items={cities}
+              onSelect={value => setCity(value)}
+              color="gray"
+              placeholder="Cidade"
+            />
+          </View>
+          <View>
+            <Text style={styles.label}>Status</Text>
+            <Dropdown
+              items={[
+                {label: 'Todos', value: 'all'},
+                {label: 'Ativo', value: 'active'},
+                {label: 'Inativo', value: 'deactive'},
+              ]}
+              onSelect={value => {
+                if (value === 'all') {
+                  setStatus('');
+                } else {
+                  setStatus(value);
+                }
+              }}
+              color="gray"
+              placeholder="Todos"
+            />
+          </View>
+        </View>
+        <View style={styles.filterBtnView}>
+          <Btn
+            onPress={() => confirmFilter()}
+            styleType="filled"
+            title="Confirmar"
+          />
+          <Btn
+            onPress={() => cancelFilter()}
+            styleType="outlined"
+            title="Limpar filtros"
+          />
+        </View>
+      </BottomModal>
+    </>
   );
 }
 
@@ -112,6 +191,34 @@ const styles = StyleSheet.create({
   },
   item: {
     marginBottom: 12,
+  },
+  filterFields: {
+    gap: 12,
+    marginVertical: 18,
+  },
+  filterBtnView: {
+    gap: 8,
+    marginTop: 10,
+  },
+  label: {
+    color: colors.dark_gray,
+    marginBottom: 4,
+  },
+  filterView: {
+    position: 'relative',
+  },
+  filterBadge: {
+    position: 'absolute',
+    fontWeight: 'bold',
+    fontSize: 12,
+    width: 22,
+    height: 22,
+    backgroundColor: colors.green_1,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    borderRadius: 8,
+    bottom: -8,
+    left: -8,
   },
 });
 
