1888d061d497f83e4ceeff5c8cbd9725466af4a0
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-10-07 15:45:04 -0300

    feat: insert navbar into toolProfile screen

--- frontend/FCManager/src/screens/ToolProfile/index.tsx
+++ frontend/FCManager/src/screens/ToolProfile/index.tsx
@@ -8,6 +8,7 @@ import {
   View,
 } from 'react-native';
 import colors from '../../styles/variables';
+
 import Header from '../../components/Header/Index';
 import ImageCarousel from '../../components/ImageCarousel';
 import Title from '../../components/Title';
@@ -15,7 +16,10 @@ import Badget from '../../components/Badget';
 import Info from '../../components/Info';
 import Btn from '../../components/Button';
 import BottomModal from '../../components/BottomModal';
+import Navbar from '../../components/Navbar';
+
 import Equipamento from '../../services/Equipamento';
+
 import {Equipamento as EquipamentoType} from '../../types';
 
 const {width, height} = Dimensions.get('window');
@@ -101,31 +105,28 @@ function ToolProfile({navigation, route}: any) {
                 <Text style={styles.value}>{equipamento?.obs as string}</Text>
               </View>
             </View>
-            <View style={styles.btnView}>
-              <View style={styles.btn}>
-                <Btn
-                  onPress={() => edit()}
-                  styleType="outlined"
-                  title="Editar"
-                />
-              </View>
-              <View style={styles.btn}>
-                {equipamento?.status === 'ativo' ? (
-                  <Btn
-                    onPress={() => deactivate()}
-                    styleType="alert"
-                    title="Desativar"
-                  />
-                ) : (
-                  <Btn
-                    onPress={() => activate()}
-                    styleType="filled"
-                    title="Ativar"
-                  />
-                )}
-              </View>
-            </View>
           </ScrollView>
+          <View style={styles.btnView}>
+            <View style={styles.btn}>
+              <Btn onPress={() => edit()} styleType="outlined" title="Editar" />
+            </View>
+            <View style={styles.btn}>
+              {equipamento?.status === 'ativo' ? (
+                <Btn
+                  onPress={() => deactivate()}
+                  styleType="alert"
+                  title="Desativar"
+                />
+              ) : (
+                <Btn
+                  onPress={() => activate()}
+                  styleType="filled"
+                  title="Ativar"
+                />
+              )}
+            </View>
+          </View>
+          <Navbar selected="Equipamentos" navigation={navigation} />
         </SafeAreaView>
 
         <BottomModal
@@ -174,11 +175,11 @@ const styles = StyleSheet.create({
   container: {
     width,
     height,
-    backgroundColor: colors.white_3,
+    backgroundColor: colors.white,
   },
   content: {
-    backgroundColor: colors.white,
     padding: 16,
+    flex: 1,
   },
   info: {
     marginTop: 18,
@@ -202,7 +203,7 @@ const styles = StyleSheet.create({
     marginVertical: 12,
     flexDirection: 'row',
     width: '100%',
-    paddingBottom: 14,
+    paddingHorizontal: 12,
     gap: 12,
   },
   btn: {
@@ -212,6 +213,9 @@ const styles = StyleSheet.create({
   confirmBtnView: {
     gap: 12,
     marginTop: 36,
+  },
+  spacing: {
+    flex: 1,
   },
 });
 
