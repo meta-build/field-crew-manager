b0e339b4a8d6d2d7cb98773579ac02290564a3fc
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-10-12 17:52:36 -0300

    feat: add inputMaskText component

---
+++ frontend/FCManager/src/components/InputMaskText/index.tsx
@@ -0,0 +1,100 @@
+import React, {useState} from 'react';
+import {Pressable, StyleProp, StyleSheet, TextStyle, View} from 'react-native';
+
+import colors from '../../styles/variables';
+
+import EyeIcon from '../../assets/icons/eye.svg';
+import ClosedEyeIcon from '../../assets/icons/closedEye.svg';
+import {MaskedTextInput, MaskedTextInputProps} from 'react-native-mask-text';
+
+interface Props extends MaskedTextInputProps {
+  color: 'gray' | 'white';
+  enable?: boolean;
+  style?: StyleProp<TextStyle>;
+  error?: boolean;
+  isPassword?: boolean;
+}
+
+const EyeComponent = ({
+  open,
+  onPress,
+}: {
+  open: boolean;
+  onPress: () => void;
+}) => {
+  const stylesEyeComponent = StyleSheet.create({
+    pressable: {
+      height: 28,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+  return (
+    <Pressable style={stylesEyeComponent.pressable} onPress={() => onPress()}>
+      {React.cloneElement(open ? <EyeIcon /> : <ClosedEyeIcon />, {
+        width: 20,
+        color: colors.dark_gray,
+      })}
+    </Pressable>
+  );
+};
+
+const InputMaskText = ({enable = true, color, ...props}: Props) => {
+  const [hideText, setHideText] = useState(true);
+
+  const styles = StyleSheet.create({
+    input: {
+      color: colors.dark_gray,
+      padding: 0,
+      flex: 1,
+      alignSelf: 'stretch',
+    },
+    gray: {
+      borderRadius: 10,
+      padding: 6,
+      backgroundColor: colors.white_2,
+      flexDirection: 'row',
+      alignItems: 'flex-start',
+      paddingHorizontal: 8,
+      ...(props.error ? {borderColor: colors.alert_1, borderWidth: 1} : {}),
+      ...(!enable ? {backgroundColor: colors.white_3} : {}),
+    },
+    white: {
+      borderRadius: 10,
+      padding: 6,
+      backgroundColor: colors.white,
+      flexDirection: 'row',
+      alignItems: 'center',
+      paddingHorizontal: 8,
+      ...(props.error ? {borderColor: colors.alert_1, borderWidth: 1} : {}),
+      ...(!enable ? {backgroundColor: colors.white_4} : {}),
+    },
+  });
+
+  return (
+    <View style={[styles[color], props.style]}>
+      <MaskedTextInput
+        onChangeText={props.onChangeText}
+        keyboardType={props.keyboardType}
+        mask={props.mask}
+        style={styles.input}
+        placeholderTextColor={colors.light_gray_2}
+        onChange={props.onChange}
+        value={props.value}
+        placeholder={props.placeholder}
+        editable={enable}
+        multiline={props.multiline}
+        textAlignVertical={props.textAlignVertical}
+        secureTextEntry={props.isPassword ? hideText : false}
+      />
+      {props.isPassword ? (
+        <EyeComponent open={!hideText} onPress={() => setHideText(!hideText)} />
+      ) : (
+        <></>
+      )}
+    </View>
+  );
+};
+
+export default InputMaskText;
