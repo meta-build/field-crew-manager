f49d1a5723960e36e2f3b7429cb6b9deb8785f85
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-10-12 12:59:18 -0300

    refact: submit functions on login modals on home screen

--- frontend/FCManager/src/screens/Home/index.tsx
+++ frontend/FCManager/src/screens/Home/index.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {
   Dimensions,
   Image,
-  KeyboardAvoidingView,
   SafeAreaView,
   StyleSheet,
   Text,
@@ -18,42 +17,75 @@ import InputText from '../../components/InputText';
 const logo = require('../../assets/images/logo-1.png');
 const image = require('../../assets/images/home-image.png');
 
-const { width, height } = Dimensions.get('window');
+const {width, height} = Dimensions.get('window');
 
-const Home = ({ navigation }: any) => {
-  const [loginModalL, setloginModal] = useState(false);
+const Home = ({navigation}: any) => {
+  const [loginMailModal, setLoginMailModal] = useState(false);
   const [loginPasswordModal, setloginPasswordModal] = useState(false);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [name, setName] = useState('');
+
   const [failEmail, setFailEmail] = useState(false);
   const [failPassword, setFailPassword] = useState(false);
 
-  const openLoginModal = () => {
-    setloginModal(true);
-    setloginPasswordModal(false);
+  const [loading, setLoading] = useState(false);
+
+  const openLoginMailModal = () => {
+    setLoginMailModal(true);
   };
-  
-  const closeLoginModal = () => {
-    setloginModal(false);
+
+  const closeLoginModals = () => {
+    setLoginMailModal(false);
+    setloginPasswordModal(false);
+    setEmail('');
+    setPassword('');
+    setFailEmail(false);
+    setFailEmail(false);
+  };
+
+  const submitEmail = () => {
+    if (email) {
+      // antes da requisição
+      setLoading(true);
+
+      // se encontrar email
+      setName('flano');
+      openLoginPasswordModal();
+
+      // se não encontrar email
+      // setFailEmail(true);
+
+      // depois da requisição
+      setLoading(false);
+    }
   };
 
   const openLoginPasswordModal = () => {
-     setloginPasswordModal(true);
-     setloginModal(false);
+    setloginPasswordModal(true);
+    setLoginMailModal(false);
   };
 
-  const closeLoginPasswordModal = () => {
-    setloginPasswordModal(false);
-  }
-
-  const submitEmail = () => {
-     setFailEmail(true);
-  }
-
   const submitPassword = () => {
-    setFailPassword(true);
-  }
+    if (password) {
+      // antes da requisição
+      setLoading(true);
+
+      // se senha correta
+      goToList();
+
+      //  se senha incorreta
+      setFailPassword(true);
+
+      // depois da requisição
+      setLoading(false);
+    }
+  };
 
   function goToList(): void {
-    throw new Error('Function not implemented.');
+    navigation.navigate('ToolList');
   }
 
   return (
@@ -68,82 +100,86 @@ const Home = ({ navigation }: any) => {
             text="Gerencie seus equipamentos em campo."
           />
           <Text style={styles.text}>
-            O segredo para o sucesso em campo: simplifique o gerenciamento de seus
-            equipamentos de forma fácil, intuitiva e eficiente.
+            O segredo para o sucesso em campo: simplifique o gerenciamento de
+            seus equipamentos de forma fácil, intuitiva e eficiente.
           </Text>
         </View>
         <View style={styles.btnContainer}>
-          <Btn onPress={() => openLoginModal()} styleType="filled" title="Iniciar" />
+          <Btn
+            onPress={() => openLoginMailModal()}
+            styleType="filled"
+            title="Iniciar"
+          />
         </View>
       </SafeAreaView>
+
+      {/* login email */}
       <BottomModal
-        visible={loginModalL}
-        onPressOutside={() =>
-          closeLoginModal()}>
-            
-        <Title
-         text='Seja bem vindo(a)'
-         color={'green'}
-         align={'left'}
-        />
-        
+        visible={loginMailModal}
+        onPressOutside={() => closeLoginModals()}>
+        <Title text="Seja bem vindo(a)" color={'green'} align={'left'} />
 
-        {failEmail ? <Text> email invalido</Text> : <></>}
-        <Text style={styles.modalText}>
-          E-mail:
-        </Text>
-
-        <InputText
-         color={'gray'}
-         placeholder='exemplo@dominio.com'
-         style={styles.inputText}
-         error={failEmail}     
-         />
-          
-
-        <View style={styles.btnContainer}> 
-         <Btn 
-          onPress={() => openLoginPasswordModal()}
-          styleType="outlined"
-          title='Continuar' />
+        <View style={styles.loginView}>
+          {failEmail ? (
+            <Text style={styles.modalError}>E-mail não encontrado.</Text>
+          ) : (
+            <></>
+          )}
+          <Text style={styles.modalLabel}>E-mail:</Text>
+          <InputText
+            color={'gray'}
+            placeholder="exemplo@dominio.com"
+            style={styles.inputText}
+            error={failEmail}
+            value={email}
+            onChange={e => setEmail(e.nativeEvent.text)}
+          />
         </View>
 
+        <View style={styles.btnContainer}>
+          <Btn
+            onPress={() => submitEmail()}
+            styleType="outlined"
+            title="Continuar"
+            loading={loading}
+          />
+        </View>
       </BottomModal>
-      
+
+      {/* login senha */}
       <BottomModal
-             visible={loginPasswordModal}
-             onPressOutside={() =>
-              closeLoginPasswordModal()}>
+        visible={loginPasswordModal}
+        onPressOutside={() => closeLoginModals()}>
+        <Title text="Seja bem-vindo(a)," color={'green'} align={'left'} />
+        <Title text={name} color={'green'} align={'left'} />
 
-          <Title
-           text='Seja bem vindo(a)'
-           color={'green'}
-           align={'left'}
-           />
-
-          {failPassword ? <Text> senha invalida</Text> : <></>}
-          <Text style={styles.modalText}>
-            Senha:
-          </Text>
+        <View style={styles.loginView}>
+          {failPassword ? (
+            <Text style={styles.modalError}>Senha incorreta.</Text>
+          ) : (
+            <></>
+          )}
+          <Text style={styles.modalLabel}>Senha:</Text>
 
           <InputText
-           color={'gray'}
-           isPassword style={styles.inputText}
-           error={failPassword}
-           > 
-           </InputText>
-
-        <View style={styles.btnContainer}> 
-         <Btn 
-          onPress={() => closeLoginPasswordModal()}
-          styleType="outlined"
-          title='Entrar' />
+            color={'gray'}
+            isPassword
+            style={styles.inputText}
+            error={failPassword}
+            placeholder="Senha"
+            onChange={e => setPassword(e.nativeEvent.text)}
+          />
         </View>
 
-
-
+        <View style={styles.btnContainer}>
+          <Btn
+            onPress={() => submitPassword()}
+            styleType="outlined"
+            title="Entrar"
+            loading={loading}
+          />
+        </View>
       </BottomModal>
-
     </>
   );
 };
@@ -180,6 +216,9 @@ const styles = StyleSheet.create({
   btnContainer: {
     width: '100%',
   },
+  loginView: {
+    marginTop: 18,
+  },
   modalText: {
     marginBottom: 8,
     marginTop: 35,
@@ -187,15 +226,14 @@ const styles = StyleSheet.create({
   inputText: {
     marginBottom: 24,
   },
-  
+  modalLabel: {
+    color: colors.dark_gray,
+    marginBottom: 6,
+  },
+  modalError: {
+    color: colors.alert_1,
+    marginVertical: 8,
+  },
 });
 
 export default Home;
-function setEmailError(arg0: boolean) {
-  throw new Error('Function not implemented.');
-}
-
-function setPasswordError(arg0: boolean) {
-  throw new Error('Function not implemented.');
-}
-
