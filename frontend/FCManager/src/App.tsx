48579ee87c4939a2a931cc150b6d15413cafad05
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-10-15 13:35:43 -0300

    refact: insert screens container in another file

--- frontend/FCManager/src/App.tsx
+++ frontend/FCManager/src/App.tsx
@@ -1,65 +1,16 @@
 import React from 'react';
 
-import {StatusBar, StyleSheet} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
-
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
-import colors from './styles/variables';
-
-import Home from './screens/Home';
-import ToolList from './screens/ToolList';
-import ToolProfile from './screens/ToolProfile';
-import NewTool from './screens/NewTool';
-import UserList from './screens/UserList';
-import UserProfile from './screens/UserProfile';
-import UserForm from './screens/UserForm';
-import OwnUserProfile from './screens/OwnUserProfile';
-import UpdateUserData from './screens/UpdateUserData';
-import ChangePassword from './screens/ChangePassword';
-import ManeuverList from './screens/ManeuverList';
-import ManeuverProfile from './screens/ManeuverProfile';
-import ManeuverForm from './screens/ManeuverForm';
-
-const Stack = createNativeStackNavigator();
+import ScreensContainer from './screens/ScreensContainer';
+import {ContextoProvider} from './contexts/Contexto';
 
 const App = () => {
   return (
     <>
-      <StatusBar backgroundColor={colors.white} />
-      <NavigationContainer>
-        <SafeAreaView style={styles.container}>
-          <Stack.Navigator
-            screenOptions={{
-              headerShown: false,
-              header: () => null,
-              navigationBarColor: colors.white,
-            }}>
-            <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="ToolList" component={ToolList} />
-            <Stack.Screen name="ToolProfile" component={ToolProfile} />
-            <Stack.Screen name="NewTool" component={NewTool} />
-            <Stack.Screen name="UserList" component={UserList} />
-            <Stack.Screen name="UserProfile" component={UserProfile} />
-            <Stack.Screen name="UserForm" component={UserForm} />
-            <Stack.Screen name="OwnUserProfile" component={OwnUserProfile} />
-            <Stack.Screen name="UpdateUserData" component={UpdateUserData} />
-            <Stack.Screen name="ChangePassword" component={ChangePassword} />
-            <Stack.Screen name="ManeuverList" component={ManeuverList} />
-            <Stack.Screen name="ManeuverProfile" component={ManeuverProfile} />
-            <Stack.Screen name="ManeuverForm" component={ManeuverForm} />
-          </Stack.Navigator>
-        </SafeAreaView>
-      </NavigationContainer>
+      <ContextoProvider>
+        <ScreensContainer />
+      </ContextoProvider>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
