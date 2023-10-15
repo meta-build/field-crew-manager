991cf661843e552f282374b1fb758d31c28d2f95
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-10-15 12:55:32 -0300

    feat: implement token on axios instance

--- frontend/FCManager/src/services/Tipo.ts
+++ frontend/FCManager/src/services/Tipo.ts
@@ -1,14 +1,14 @@
 import {Tipo as TipoType} from '../types/index';
-import {apiJson} from './api';
+import api from './api';
 
 class Tipo {
   async getAll(): Promise<TipoType[]> {
-    const {data} = await apiJson.get('/tipos');
+    const {data} = await api.apiJson.get('/tipos');
     return data;
   }
 
   async new(value: string): Promise<{id: string}> {
-    const {data} = await apiJson.post('/tipos', {value});
+    const {data} = await api.apiJson.post('/tipos', {value});
     return data;
   }
 }
