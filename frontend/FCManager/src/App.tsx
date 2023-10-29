0d681ae34811fb573c39bd7fc2228c5f75e24f6d
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-10-28 21:54:25 -0300

    build: react-native-maps instalantion and configuration

--- frontend/FCManager/src/App.tsx
+++ frontend/FCManager/src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import {enableLatestRenderer} from 'react-native-maps';
+
 import ScreensContainer from './screens/ScreensContainer';
 import {ContextoProvider} from './contexts/Contexto';
+
+enableLatestRenderer();
 
 const App = () => {
   return (
