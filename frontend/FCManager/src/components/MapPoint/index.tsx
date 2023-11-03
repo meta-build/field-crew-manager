e638d31a7beb6e972d76e392e4ff24b344e15a02
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-11-03 15:03:27 -0300

    feat: change view to image in mapPoint component

--- frontend/FCManager/src/components/MapPoint/index.tsx
+++ frontend/FCManager/src/components/MapPoint/index.tsx
@@ -1,45 +1,37 @@
-import React from 'react';
-import {Pressable, StyleSheet} from 'react-native';
-import colors from '../../styles/variables';
+import React, {useEffect} from 'react';
+import {Image, StyleSheet} from 'react-native';
 
 interface Props {
   color: 'green' | 'gray';
-  expanded?: boolean;
+  selected?: boolean;
   onPress?: () => void;
 }
 
+const grayIcon = require('../../assets/images/map-icon-gray.png');
+const greenIcon = require('../../assets/images/map-icon-green.png');
+const blueIcon = require('../../assets/images/map-icon-blue.png');
+
 function MapPoint(props: Props) {
+  useEffect(() => console.log('aqui'));
   return (
-    <Pressable
-      style={[
-        styles.point,
-        styles[props.color],
-        props.expanded ? styles.expanded : {},
-      ]}
-      onPress={() => props.onPress && props.onPress()}
+    <Image
+      style={styles.normal}
+      resizeMode="contain"
+      source={
+        props.selected
+          ? blueIcon
+          : props.color === 'green'
+          ? greenIcon
+          : grayIcon
+      }
     />
   );
 }
 
 const styles = StyleSheet.create({
-  point: {
+  normal: {
     width: 18,
     height: 18,
-    borderRadius: 18,
-    borderWidth: 3,
-  },
-  expanded: {
-    width: 28,
-    height: 28,
-    borderWidth: 5,
-  },
-  green: {
-    backgroundColor: colors.green_1,
-    borderColor: colors.green_3,
-  },
-  gray: {
-    backgroundColor: colors.gray,
-    borderColor: colors.dark_gray,
   },
 });
 
