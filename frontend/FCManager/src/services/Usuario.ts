fd44c8f544d3e57c4edd26301dab4ebf1fcbeb21
RodrigoDGoulart <rodinizgoulart@gmail.com>
2023-11-26 15:53:59 -0300

    feat: confirm send code to email

--- frontend/FCManager/src/services/Usuario.ts
+++ frontend/FCManager/src/services/Usuario.ts
@@ -177,6 +177,10 @@ class Usuario {
     await AsyncStorage.removeItem('token');
     await AsyncStorage.removeItem('usuario');
   }
+
+  async sendCode() {
+    console.log('mandando código');
+  }
 }
 
 export default new Usuario();
